f2fe098fb09e022426d0ce5f9099ff348d23f0a0
pateldrashti2227 <pateldrashti2227@gmail.com>
2026-08-15 10:09:52 +0530

    practical-05

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PasswordInput from "./Componet/PaaswordInput";
+
+function App() {
+  return (
+    <div>
+      <PasswordInput />
+    </div>
+  );
+}
+
+export default App;
